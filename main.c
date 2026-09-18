8d9ce63440804377f3bccaad3941b0427ff73e37
krema <bugthemonke@gmail.com>
2026-09-18 13:21:47 +0200

    commit

--- main.c
+++ main.c
@@ -10,39 +10,35 @@ char *getl();
 int main(void) {
 	printf(SCR_ENT);
 
-    printf("\n\n--------------------------------\n");
+    	printf("\n\n--------------------------------\n");
 	printf("--| MAKEMAKE: Makefile maker |--\n");
 	printf("--------------------------------\n\n");
 	
 	printf("WARNING: This will overwrite any file name \"makefile\" or the file name you choose.");
 	printf("Only GCC style compilers are supported, if your compiler doesnt support \"-o\" the same way GCC does, its not supported.\n\n\n");
 
-	printf("Makefile name (default: makefile) : "); char *MAKEFILE = getl();
+	printf("Compiler of choice : "); char *compilerName = getl();
 
-	printf("Compiler of choice : "); char *CC = getl();
+	printf("Source file : "); char *sourceFileName = getl();
 
-	printf("Source file : "); char *SRC = getl();
+	printf("Output filename : "); char *outputFileName = getl();
 
-	printf("Output filename : "); char *OUT = getl();
+	printf("Extra flags : "); char *compileFlags = getl();
 
-	printf("Extra flags : "); char *FLAGS = getl();
+	printf("Add a \"make clean\" function [y/N] : "); char CLEAN = getchar();
 
-	printf("add a \"make clean\" function [y/N] : "); char CLEAN = getchar();
-
-	FILE *makefile = fopen(MAKEFILE, "w");
+	FILE *makefile = fopen("makefile", "w");
 
 	fprintf(makefile, "# MADE BY MakeMake\n");
 
-	fprintf(makefile, "CC = %s\n", CC);
-	fprintf(makefile, "SRC = %s\n", SRC);
-	fprintf(makefile, "OUT = %s\n", OUT);
-	fprintf(makefile, "FLAGS = %s\n\n", FLAGS);
+	fprintf(makefile, "CC = %s\n", compilerName);
+	fprintf(makefile, "SRC = %s\n", sourceFileName);
+	fprintf(makefile, "OUT = %s\n", outputFileName);
+	fprintf(makefile, "FLAGS = %s\n\n", compileFlags);
 
-	fprintf(makefile, "all: %s\n", OUT);
-	fprintf(makefile, "%s:\n", OUT);
+	fprintf(makefile, "all: %s\n", outputFileName);
+	fprintf(makefile, "%s:\n", outputFileName);
 	fprintf(makefile, "\t$(CC) $(FLAGS) $(SRC) -o $(OUT)");
-
-	if (MAKEFILE == '\n') MAKEFILE = "makefile";
 
 	if (CLEAN == 'y' || CLEAN == 'Y') {
 		fprintf(makefile, "\n\n.PHONY: clean\n");
@@ -52,13 +48,14 @@ int main(void) {
 
 	printf("\"./makefile\" written. if you have any issues or can help with other compilers reach out to my discord: @kremadev");
 
+	getchar();
+
 	printf(SCR_EXT);
 
-	free(MAKEFILE);
-	free(CC);
-	free(SRC);
-	free(OUT);
-	free(FLAGS);
+	free(compilerName);
+	free(sourceFileName);
+	free(outputFileName);
+	free(compileFlags);
 
 	return 0;
 }
